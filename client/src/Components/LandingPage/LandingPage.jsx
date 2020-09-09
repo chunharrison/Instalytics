4521b9781d3a7df5451fa9591dfac2cf4d633499
MarioH95 <harb.mario@gmail.com>
2020-09-09 16:56:45 -0300

    added front end design

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,17 +1,59 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react' 
+import { ResponsiveContainer, LineChart, Line } from 'recharts';
 
 import axios from 'axios'
+
+import './LandingPage.css';
 
 const LandingPage = props => {
     const [loggedIn, setLoggedIn] = useState(false)
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [currentUser, setCurrentUser] = useState('')
+    const canvasElement = useRef(null);
+    const [context, setContext] = useState(null)
+    const [searched, setSearched] = useState(false);
+    const [data, setData] = useState([]);
+    let time = 0;
 
     useEffect(() => {
+        setContext(canvasElement.current.getContext('2d'));
         const users = localStorage.getItem('Instalytics_Users');
-        if (users !== null && users !== []) setLoggedIn(true)
+        if (users !== null && users !== []) setLoggedIn(true);
     }, [])
+
+    useEffect(() => {
+        if (!context) return;
+        startAnimation();
+    }, [context])
+
+    function color (x, y, r, g, b) {
+        if (!canvasElement || !context) return;
+        context.fillStyle = `rgb(${r}, ${g}, ${b})`
+        context.fillRect(x, y, 10, 10);
+    }
+
+    function R (x, y, time) {
+        return (Math.floor(200 + 35 * Math.cos((x * x - y * y) / 300 + time)));
+    }
+
+    function G (x, y, time) {
+        return (Math.floor(130 + 40 * Math.cos((x * x - y * y) * Math.cos(time / 3)) / 300));
+    }
+
+    function B (x, y, time) {
+        return (Math.floor(136 + 17 * Math.sin(5 * Math.sin(time / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
+    }
+
+    function startAnimation() {
+        for (let x = 0; x <= 31; x++) {
+            for (let y = 0; y <= 31; y++) {
+                color(x, y, R(x, y, time), G(x, y, time), B(x, y, time));
+            }
+        }
+        time = time + 0.02;
+        window.requestAnimationFrame(startAnimation);
+    }
 
     function handleAccountChange(e) {
         setUsername(e.target.value)
@@ -24,6 +66,8 @@ const LandingPage = props => {
     function handleSubmit(e) {
         e.preventDefault()
         
+        setSearched(true);
+
         const options = {
             params: {
                 login_user: username,
@@ -70,8 +114,7 @@ const LandingPage = props => {
     }
 
     // GET DATA
-    function followers_to_likes(e) {
-        e.preventDefault()
+    function followers_to_likes() {
 
         const getDataOptions = {
             params: {
@@ -86,7 +129,7 @@ const LandingPage = props => {
         }
         axios.get('http://localhost:5000/api/followers_to_likes', getDataOptions)
             .then(res => {
-                console.log(res)
+                console.log(res);
             })
     }
 
@@ -106,7 +149,7 @@ const LandingPage = props => {
         }
         axios.get('http://localhost:5000/api/followers_to_comments', getDataOptions)
             .then(res => {
-                console.log(res)
+                
             })
     }
 
@@ -126,13 +169,11 @@ const LandingPage = props => {
         }
         axios.get('http://localhost:5000/api/followers_to_views', getDataOptions)
             .then(res => {
-                console.log(res)
+                
             })
     }
 
-    function average_likes(e) {
-        e.preventDefault()
-
+    function average_likes() {
         const getDataOptions = {
             params: {
                 username: username
@@ -167,7 +208,7 @@ const LandingPage = props => {
         }
         axios.get('http://localhost:5000/api/average_comments', getDataOptions)
             .then(res => {
-                console.log(res)
+                
             })
     }
     
@@ -187,7 +228,7 @@ const LandingPage = props => {
         }
         axios.get('http://localhost:5000/api/average_views', getDataOptions)
             .then(res => {
-                console.log(res)
+                
             })
     }
 
@@ -198,52 +239,38 @@ const LandingPage = props => {
         console.log(users)
     }
 
-    return (<div>
-        {/* <form onSubmit={e => handleSubmit(e)}>
-                <label>
-                    Username:
-                <input type="text" value={username} onChange={e => handleAccountChange(e)}/>
-                </label>
-                <label>
-                    Password:
-                <input type="text" value={password} onChange={e => handlePasswordChange(e)}/>
-                </label>
-                <input type="submit" value="Submit" />
-          </form>
-          <div>
-                <button onClick={e => followers_to_likes(e)}>followers to likes</button>
-                <button onClick={e => followers_to_comments(e)}>followers to comments</button>
-                <button onClick={e => followers_to_views(e)}>followers to views (videos)</button>
-                <button onClick={e => average_likes(e)}>average likes</button>
-                <button onClick={e => average_comments(e)}>average comments</button>
-                <button onClick={e => average_views(e)}>average views</button>
-            </div> */}
-            <button onClick={e => getLocalStorage(e)}>get localStorage</button>
-        {
-            loggedIn
-            ?   <div>
-                <button onClick={e => followers_to_likes(e)}>followers to likes</button>
-                <button onClick={e => followers_to_comments(e)}>followers to comments</button>
-                <button onClick={e => followers_to_views(e)}>followers to views (videos)</button>
-                <button onClick={e => average_likes(e)}>average likes</button>
-                <button onClick={e => average_comments(e)}>average comments</button>
-                <button onClick={e => average_views(e)}>average views</button>
-                </div>
-            :
-            <form onSubmit={e => handleSubmit(e)}>
-                <label>
-                    Username:
-                <input type="text" value={username} onChange={e => handleAccountChange(e)}/>
-                </label>
-                <label>
-                    Password:
-                <input type="text" value={password} onChange={e => handlePasswordChange(e)}/>
-                </label>
-                <input type="submit" value="Submit" />
-          </form>
+    return (
+        <div className='search-page-container'>
+            <div className='nav'>
+                <p className='nav-logo'>instalytics</p>
+            </div>
+            <div className='search-page-search-container' style={{height: `${searched ? '60px' : ''}`}}>
+                <canvas ref={canvasElement} id="canvas" width='32px' height={`${searched ? '5px' : '32px'}`} style={{height: `${searched ? '60px' : ''}`, opacity: `${searched ? '0.7' : ''}`}}/>
+                {searched ? 
+                <label className='search-page-username-label'>{username}</label>
+                : 
+                <form onSubmit={e => handleSubmit(e)} className='search-page-form'>
+                    <input className='search-page-input' type="text" value={username} placeholder='Username' onChange={e => handleAccountChange(e)}/>
+                    <input className='search-page-input' type="password" value={password} placeholder='Password' onChange={e => handlePasswordChange(e)}/>
+                    <button className='search-page-submit' type="submit">Analyze</button>
+                </form>
+                }
+            </div>
+            <div className='search-page-data-container' style={{display: `${searched ? '' : 'none'}`}}>
+                <ResponsiveContainer className='search-page-data' width={700} height="35%">
+                    <LineChart data={data}>
+                        <Line type="monotone" dataKey="uv" stroke="#8884d8" isAnimationActive={false}/>
+                    </LineChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer className='search-page-data' width={700} height="35%">
+                <LineChart data={data} >
+                    <Line type="monotone" dataKey="likes" stroke="#8884d8" isAnimationActive={false}/>
+                </LineChart>
+                </ResponsiveContainer>
 
-        }
-    </div>)
+            </div>
+        </div>
+    )
 }
 
 export default LandingPage

8ae77911096d7b58c9756e7cbf3def4dd13e1c8f
harrisonchun <h7chun@uwaterloo.ca>
2020-09-11 19:20:36 -0300

    Added email notification, username check and query param handle

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -4,17 +4,33 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 import axios from 'axios'
+import validator from 'validator';
+import {isEmpty} from 'is-empty'
+import queryString from 'query-string';
 
 import './LandingPage.css';
 
 const LandingPage = props => {
     const [loggedIn, setLoggedIn] = useState(false)
-    const [username, setUsername] = useState('cosign.pro')
+
+    // login input 1
+    const [username, setUsername] = useState('')
+    const [usernameErrorMessage, setUsernameErrorMessage] = useState('')
     const [password, setPassword] = useState('')
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState('')
+    const [email, setEmail] = useState('')
+    const [sendEmail, setSendEmail] = useState(false)
+    const [emailErrorMessage, setEmailErrorMessage] = useState('')
+    // login input 2
+    const [usernameCheck, setUsernameCheck] = useState('')
+    const [usernameCheckErrorMessage, setUsernameCheckErrorMessage] = useState('')
+    const [usernameCheckResult, setUsernameCheckResult] = useState('')
+
+
     const [currentUser, setCurrentUser] = useState('')
     const canvasElement = useRef(null);
     const [context, setContext] = useState(null)
-    const [searched, setSearched] = useState(true);
+    const [searched, setSearched] = useState(false);
     const [data, setData] = useState([]);
     const [activeIndex, setActiveIndex] = useState(0);
     const [activeData, setActiveData] = useState(0);
@@ -25,6 +41,13 @@ const LandingPage = props => {
     useEffect(() => {
         setContext(canvasElement.current.getContext('2d'));
         const users = localStorage.getItem('Instalytics_Users');
+
+        console.log(queryString.parse(window.location.search))
+        let usernameQuery = '' + queryString.parse(window.location.search).username
+        if (usernameQuery !== 'undefined' && usernameQuery !== undefined
+         && usernameQuery !== null && usernameQuery !== '') {
+            handleQueryParamUsername(usernameQuery)
+        }
     }, [])
 
     useEffect(() => {
@@ -84,10 +107,23 @@ const LandingPage = props => {
         setPassword(e.target.value)
     }
 
-    function getData() {
+    function handleEmailChange(e) {
+        setEmail(e.target.value)
+    }
+
+    function handleEmailCheckboxChange(e) {
+        console.log(sendEmail)
+        setSendEmail(!sendEmail)
+    }
+
+    function handleAccountCheckChange(e) {
+        setUsernameCheck(e.target.value)
+    }
+
+    function getData(uName) {
         const getDataOptions = {
             params: {
-                username: username
+                username: uName
             },
             headers: {
                 'Access-Control-Allow-Credentials': true,
@@ -99,6 +135,7 @@ const LandingPage = props => {
         axios.get('http://localhost:5000/api/start_instalytics', getDataOptions)
             .then(res => {
                 let sortedData = res.data.sort(compare);
+                console.log(sortedData)
                 setData(sortedData);
                 setActiveData(sortedData[0]);
             })
@@ -107,10 +144,115 @@ const LandingPage = props => {
     function handleSubmit(e) {
         e.preventDefault()
 
+        let goodToGo = true
+
+        // check username
+        if (username === '') {
+            goodToGo = false
+            setUsernameErrorMessage("Username field is required")
+        } else {
+            setUsernameErrorMessage("")
+        }
+
+        // check password
+        if (password === '') {
+            goodToGo = false
+            setPasswordErrorMessage("Password field is required")
+        } else {
+            setPasswordErrorMessage('')
+        }
+
+        // check email
+        if (sendEmail) {
+            if (email === '') {
+                goodToGo = false
+                setEmailErrorMessage("Email field is required")
+            } else if (!validator.isEmail(email)) {
+                goodToGo = false
+                setEmailErrorMessage("Email is invalid")
+            } else {
+                setEmailErrorMessage('')
+            }
+        } else {
+            setEmailErrorMessage('')
+        }
+        
+        
+        if (goodToGo) {
+            const options = {
+                params: {
+                    login_user: username,
+                    login_pass: password,
+                    sendEmail: sendEmail,
+                    email: email
+                },
+                headers: {
+                    'Access-Control-Allow-Credentials': true,
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET',
+                    'Access-Control-Allow-Headers': '*',
+                },
+            }
+
+            console.log('calling store-metadata')
+            axios.get('http://localhost:5000/api/store-metadata', options)
+                .then(res => {
+                    // setSearched(true);
+                    // getData(username);
+                    window.location.replace(`http://localhost:3000/?username=${username}`)
+                    
+                    
+            }).catch(res => {
+                if (res.response) {
+                    console.log(res.response.data.message)
+                } else {
+                    console.log(res)
+                }
+            })
+        } else {
+            console.log("we need something")
+        }
+    }
+
+    function handleSubmit2 (e) {
+        e.preventDefault()
+
+        // reset
+        setUsernameCheckResult('')
+
+        if (usernameCheck === '' ){
+            setUsernameCheckErrorMessage("Username field is required")
+        } else {
+            const options = {
+                params: {
+                    username: usernameCheck,
+                },
+                headers: {
+                    'Access-Control-Allow-Credentials': true,
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET',
+                    'Access-Control-Allow-Headers': '*',
+                },
+            }
+
+            axios.get('http://localhost:5000/api/check-username', options)
+                .then(res => {
+                    console.log('handleSubmit2 GOOD')
+                    setUsernameCheckResult('good')
+                })
+                .catch(res => {
+                    console.log('no valid username in param')
+                    setUsernameCheckResult('bad')
+                    setUsernameCheckErrorMessage('We could not find data under the given username, please login')
+                })
+        }
+    }
+
+    function handleQueryParamUsername(usernameQuery) {
+        console.log('handleQueryParamUsername', usernameQuery)
         const options = {
             params: {
-                login_user: username,
-                login_pass: password
+                username: usernameQuery,
             },
             headers: {
                 'Access-Control-Allow-Credentials': true,
@@ -120,27 +262,16 @@ const LandingPage = props => {
             },
         }
 
-        console.log('calling store-metadata')
-        axios.get('http://localhost:5000/api/store-metadata', options)
+        axios.get('http://localhost:5000/api/check-username', options)
             .then(res => {
+                setUsername(usernameQuery)
                 setSearched(true);
-                getData();
-                // add username to the username list in localStorage 
-                const user = localStorage.getItem('Instalytics_User');
-                if (user === null || user === '') {
-                    localStorage.setItem('Instalytics_User', username);
-                } else {
-                    // check to see if the backend has the data corresponding to the username in the localstorage
-                }
-                
-                
-        }).catch(res => {
-            if (res.response) {
-                console.log(res.response.data.message)
-            } else {
-                console.log(res)
-            }
-        })
+                getData(usernameQuery);
+            })
+            .catch(res => {
+                console.log('no valid username in param')
+                setSearched(false);
+            })
     }
 
     function refresh(e) {
@@ -158,136 +289,7 @@ const LandingPage = props => {
             },
         }
     }
-
-    // add user
-    function addUser(e) {
-        e.preventDefault()
-    }
-
-    // remove user
-    function removeUser(e) {
-        e.preventDefault()
-    }
-
-    // GET DATA
-    function followers_to_likes() {
-
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        axios.get('http://localhost:5000/api/followers_to_likes', getDataOptions)
-            .then(res => {
-                console.log(res);
-            })
-    }
-
-    function followers_to_comments(e) {
-        e.preventDefault()
-
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        axios.get('http://localhost:5000/api/followers_to_comments', getDataOptions)
-            .then(res => {
-                
-            })
-    }
-
-    function followers_to_views(e) {
-        e.preventDefault()
-
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        axios.get('http://localhost:5000/api/followers_to_views', getDataOptions)
-            .then(res => {
-                
-            })
-    }
-
-    function average_likes() {
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        console.log('average_likes requested!')
-        axios.get('http://localhost:5000/api/average_likes', getDataOptions)
-            .then(res => {
-                console.log(res)
-            })
-    }
-
-    function average_comments(e) {
-        e.preventDefault()
-
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        axios.get('http://localhost:5000/api/average_comments', getDataOptions)
-            .then(res => {
-                
-            })
-    }
     
-    function average_views(e) {
-        e.preventDefault()
-
-        const getDataOptions = {
-            params: {
-                username: username
-            },
-            headers: {
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET',
-                'Access-Control-Allow-Headers': '*',
-            },
-        }
-        axios.get('http://localhost:5000/api/average_views', getDataOptions)
-            .then(res => {
-                
-            })
-    }
-
     function getLocalStorage(e) {
         e.preventDefault()
 
@@ -306,7 +308,31 @@ const LandingPage = props => {
         setActiveValue(e.value)
     }
 
-    console.log(data)
+    function handleWelcomeBackClick(e) {
+        e.preventDefault()
+
+        window.location.replace(`http://localhost:3000/?username=${usernameCheck}`)
+    }
+
+    function testEmail(e) {
+        e.preventDefault()
+
+        const options = {
+            params: {
+                email: 'gethannahbakered@gmail.com',
+                uname: username
+            },
+            headers: {
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': '*',
+            },
+        }
+
+        axios.get('http://localhost:5000/api/test-email', options)
+    }
+
     let max = Math.max.apply(Math, data.map(function(entry) { if (entry[activeValue] !== 'NaN') {return Number(entry[activeValue]);} else {return 0} }))
     const options = [
         { value: 'averageLikes', label: 'Average Likes' },
@@ -326,11 +352,43 @@ const LandingPage = props => {
                 {searched ? 
                 <label className='search-page-username-label'>{username}</label>
                 : 
-                <form onSubmit={e => handleSubmit(e)} className='search-page-form'>
-                    <input className='search-page-input' type="text" value={username} placeholder='Username' onChange={e => handleAccountChange(e)}/>
-                    <input className='search-page-input' type="password" value={password} placeholder='Password' onChange={e => handlePasswordChange(e)}/>
-                    <button className='search-page-submit' type="submit">Analyze</button>
-                </form>
+                <div>
+
+                    <form onSubmit={e => handleSubmit(e)} className='search-page-form'>
+                        <input className='search-page-input' type="text" value={username} placeholder='Username' onChange={e => handleAccountChange(e)}/>
+                        <span>{usernameErrorMessage}</span>
+                        <input className='search-page-input' type="password" value={password} placeholder='Password' onChange={e => handlePasswordChange(e)}/>
+                        <span>{passwordErrorMessage}</span>
+                        <div>
+                            <br/>
+                            <p>If you choose to fetch a lot of posts, it will take a while</p>
+                            <p>You can opt to get off the website and have us email you when the data is ready</p>
+                        </div>
+                        <input className='search-page-input' type="checkbox" checked={sendEmail} onChange={e => handleEmailCheckboxChange(e)}/>
+                        <span>Yes, send me an email notification when its done</span> 
+                        <input className='search-page-input' type="email" value={email} placeholder='Email' onChange={e => handleEmailChange(e)} disabled={sendEmail ? "" : "disabled"}/>
+                        <span>{emailErrorMessage}</span>
+                        <button className='search-page-submit' type="submit">Analyze</button>
+                    </form>
+
+                    <form onSubmit={e => handleSubmit2(e)} className='search-page-form'>
+                        <p>If you have used our service before, then start straight away</p>
+                        <input className='search-page-input' type="text" value={usernameCheck} placeholder='Username' onChange={e => handleAccountCheckChange(e)}/>
+                        <span>{usernameCheckErrorMessage}</span>
+                        <button className='search-page-submit' type="submit">Find</button>
+                    </form>
+                    {
+                        usernameCheckResult === 'good'
+                        ?
+                        <div>
+                            <p>Welcome Back {usernameCheck}</p>
+                            <button onClick={e => handleWelcomeBackClick(e)}>Go see data</button>
+                        </div>
+                        :
+                        null
+                    }
+
+                </div>
                 }
             </div>
             <div className='search-page-data-container' style={{display: `${searched ? '' : 'none'}`}}>
@@ -360,7 +418,7 @@ const LandingPage = props => {
                 <div>likes to views ratio: {activeData.LVR}</div>
                 <div>likes to follower ratio: {activeData.LFR}</div>
                 <div>likes to comments ratio: {activeData.LCR}</div>
-                <button onClick={e => getData()}>getData</button>
+                <button onClick={e => testEmail(e)}>email</button>
             </div>
         </div>
     )
